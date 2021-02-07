adfb53e3b10a84b6594058dc75eeb5caa297b1ec
Katrina Villanueva <klgvillanueva@gmail.com>
2021-02-07 13:49:00 -0500

    completed useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+// useState - function that returns an array [value, function]
+// always starts with 'use'
+// component with hook must be uppercase
+// must be in function/component body
+// cannot call conditionally
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'))
+  // onst value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState('Random Title');
+
+  const handleClick = () => {
+    if (text === 'Random Title') setText('Hello World!');
+    else setText('Random Title');
+  };
+
+  return (
+    <Fragment>
+      <h2>{text}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // hook
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 24,
+    message: 'random message',
+  });
+
+  const [name, setName] = useState('Peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('random message');
+  // function
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'new message' });
+    setMessage('Hello World!');
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
